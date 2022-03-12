fe9ab2dc802b7bc0d3f85b4fe719b18e7c9547f7
tachien2003 <66167308+tachien2003@users.noreply.github.com>
2022-03-12 16:08:27 +0700

    Code bài 1

---
+++ count_IsPrime_v2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+bool Is_Prime ( int n )
+{
+    if (n <= 1)return false;
+    for (int i = 2; i < n; i++ )
+     if (n % i == 0)
+      return false;
+}
+int main()
+{   int count = 0;
+    int n;
+    bool check = true;
+    cin >> n;
+    if (n <=1 ) check = false;
+    for (int i = 2; i < n; i++)
+    {
+      if (Is_Prime( i))
+      {
+        count ++;
+        if (n % i == 0)
+          check = false;
+      }
+    }
+   if (check) cout << n << " is Prime number" << endl;
+   else cout << n << " is not Prime number" << endl;
+   cout << "Count = " << count;
+
+}
+
+
